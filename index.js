c3747f20fe6e471e7d96ecb6cbb2050abf90e3c2
jmerges bootcamp <jamesmerges1@gmail.com>
2021-01-29 15:55:55 -0800

    Set up initial inquirer menu

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
 var inquirer = require("inquirer");
-var mysql = require("mysql");
-var cTable = rquire("console.table");
+var cTable = require("console.table");
+var connection = require("connection.js");
+const { listenerCount } = require("./connection");
+const { allowedNodeEnvironmentFlags } = require("process");
+
+function startMenu () {
+    inquirer.prompt([{
+        type: list,
+        message: "What would you like to do?",
+        name: "action",
+        choices: ["Add Employee", "Add Department", "Add Role", "View Employees", "View Departments", "View Roles", "Update an Employee's Role"]
+    }]).then(answer => {
+        switch (answer.action) {
+            case "Add Employee":
+                addEmployee();
+                break;
+            case "Add Department":
+                addDepartment();
+                break;
+            case "Add Role":
+                addRole();
+                break;
+            case "View Employees":
+                view("employees");
+                break;
+            case "View Departments":
+                view("departments");
+                break;
+            case "View Roles":
+                view("roles");
+                break;
+            case "Update an Employee's Role":
+                updateEmployeeRole();
+                break;
+        }
+    });
+}
+
+function view(table) {
+    switch (table) {
+        case "employees":
+            
+    }
+}
